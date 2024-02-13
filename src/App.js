7f514eec4c5eb2f50dabfbac3c552e52799c26f3
maureentullos <Mswalsh220@gmail.com>
2024-02-13 13:51:54 -0500

    Added search engine (lesson 4).

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo img-fluid"
-          alt="logo"
-          
-        />
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        
       </header>
+        <main>
+          <Dictionary />
+        </main>
+      <footer className="text-center">Coded by Maureen Tullos</footer>
     </div>
   );
 }
